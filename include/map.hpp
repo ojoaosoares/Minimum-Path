8102a714561d354c6c8f79bb4177cc2a02d3b375
joaosoares <ojoaosoaressantos@gmail.com>
2024-11-23 14:05:13 -0300

    Feat: header map.

---
+++ include/map.hpp
@@ -0,0 +1,15 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+typedef long long ll;
+
+vector<vector<ll>> readMap(const string &pathfile);
+
+#endif
