3b41a1a50eb1b1c0b16a0038c23f3e8ba02a4b8e
joaosoares <ojoaosoaressantos@gmail.com>
2024-11-23 14:52:02 -0300

    Feat: BFS header.

---
+++ include/bfs.hpp
@@ -0,0 +1,15 @@
+#ifndef BPS_HPP
+#define BFS_HPP
+
+#include <string>
+#include <utility>
+#include <vector>
+#include <tuple>
+
+typedef long long ll;
+
+using namespace std;
+
+pair<ll, vector<ll>> bfs(const vector<vector<char>> &map, ll x, ll y);
+
+#endif
