4282f1c921284e72417f65a47b93639c3a7e4b72
joaosoares <ojoaosoaressantos@gmail.com>
2024-11-23 18:18:33 -0300

    Feat: optmizing bfs.

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -1,12 +1,15 @@
 #include "bfs.hpp"
 #include <queue>
-#include <stack>
+#include <algorithm>
 #include "map.hpp"
 
 pair<double, vector<pair<ull, ull>>> bfs(const vector<vector<char>> &map, ull x1, ull y1, ull x2, ull y2) {
     // Input: (Graph_Ad_List) Graph, (int) the start vertice
     // Complexity: O(V + E)
     // Output: (bool) true if all vertices were reached, (vector) a vector containg all vertices predecessors, (int) the fullDistance
+
+    if (map[y1-1][x1-1] == WALL || map[y2-1][x2-1] == WALL)
+        throw invalid_argument("Coordinates invalid");
 
     ull rows = map.size(), cols = map[0].size();
 
@@ -26,71 +29,49 @@ pair<double, vector<pair<ull, ull>>> bfs(const vector<vector<char>> &map, ull x1
         while (!vertexQueue.empty())
         {
             bool found = false;
-            
+            vector<pair<ull, ull>> layerVertices;
+
             while(!vertexQueue.empty())
             {
-                pair<ull, ull> coor = vertexQueue.front();
+                layerVertices.push_back(vertexQueue.front());
                 vertexQueue.pop();
-
+            }
+            
+            for (pair<ull, ull> &coor : layerVertices)
+            {
                 coor.first--; coor.second--;
                 // To follow the matrix index
 
+                vector<pair<ull, ull>> explore;
+
                 if (coor.first > 0)
-                    if (map[coor.first - 1][coor.second] != WALL && !path[coor.first - 1][coor.second].first && !path[coor.first - 1][coor.second].second) {
+                    explore.push_back(pair<ull, ull>(coor.first - 1, coor.second));
 
-                        path[coor.first - 1][coor.second] = pair<ull, ull>(coor.first + 1, coor.second + 1);
-
-                        if (coor.first - 1 + 1 == y2 && coor.second + 1 == x2)
-                        {
-                            found = true;
-                            break;
-                        }
-
-                        vertexQueue.push(pair<ull, ull>(coor.first - 1 + 1, coor.second + 1));
-                    }
-                        
-                    
                 if (coor.first + 1 < rows)
-                    if (map[coor.first + 1][coor.second] != WALL && !path[coor.first + 1][coor.second].first && !path[coor.first + 1][coor.second].second)
-                    {
-                        path[coor.first + 1][coor.second] = pair<ull, ull>(coor.first + 1, coor.second + 1);
-
-                        if (coor.first + 1 + 1 == y2 && coor.second + 1 == x2)
-                        {
-                            found = true;
-                            break;
-                        }
-
-                        vertexQueue.push(pair<ull, ull>(coor.first + 1 + 1, coor.second + 1));
-                    }
+                    explore.push_back(pair<ull, ull>(coor.first + 1, coor.second));
 
                 if (coor.second > 0)
-                    if (map[coor.first][coor.second - 1] != WALL && !path[coor.first][coor.second - 1].first && !path[coor.first][coor.second - 1].second)
-                    {
-                        path[coor.first][coor.second - 1] = pair<ull, ull>(coor.first + 1, coor.second + 1);
-
-                        if (coor.first + 1 == y2 && coor.second - 1 + 1 == x2)
-                        {
-                            found = true;
-                            break;
-                        }
-
-                        vertexQueue.push(pair<ull, ull>(coor.first + 1, coor.second - 1 + 1));
-                    }
+                    explore.push_back(pair<ull, ull>(coor.first, coor.second - 1));
 
                 if (coor.second + 1 < cols)
-                    if (map[coor.first][coor.second + 1] != WALL && !path[coor.first][coor.second + 1].first && !path[coor.first][coor.second + 1].second)
-                    {
-                        path[coor.first][coor.second + 1] = pair<ull, ull>(coor.first + 1, coor.second + 1);
+                    explore.push_back(pair<ull, ull>(coor.first, coor.second + 1));
 
-                        if (coor.first + 1 == y2 && coor.second + 1 + 1 == x2)
+
+                for (pair<ull, ull> exCoor : explore)
+                {
+                    if (map[exCoor.first][exCoor.second] != WALL && !path[exCoor.first][exCoor.second].first && !path[exCoor.first][exCoor.second].second) {
+
+                        path[exCoor.first][exCoor.second] = pair<ull, ull>(coor.first + 1, coor.second + 1);
+
+                        if (exCoor.first + 1 == y2 && exCoor.second + 1 == x2)
                         {
                             found = true;
                             break;
                         }
 
-                        vertexQueue.push(pair<ull, ull>(coor.first + 1, coor.second + 1 + 1));
+                        vertexQueue.push(pair<ull, ull>(exCoor.first +  1, exCoor.second + 1));
                     }
+                }
             }
 
             if (found) break;
@@ -104,8 +85,6 @@ pair<double, vector<pair<ull, ull>>> bfs(const vector<vector<char>> &map, ull x1
     double pathSize = 0;
 
     vector<pair<ull, ull>> finalPath;
-
-    stack<pair<ull, ull>> pathAux;
 
     while (path[curY - 1][curX - 1].first != curY || path[curY - 1][curX - 1].second != curX)
     {
@@ -127,22 +106,15 @@ pair<double, vector<pair<ull, ull>>> bfs(const vector<vector<char>> &map, ull x1
                 break;
         }
 
-        pathAux.push(pair<ull, ull>(curX, curY));
+        finalPath.push_back(pair<ull, ull>(curX, curY));
 
         curY = path[curY - 1][curX - 1].first;
         curX = path[curY - 1][curX - 1].second;
     }
 
-    pathAux.push(pair<ull, ull>(curX, curY));
+    finalPath.push_back(pair<ull, ull>(curX, curY));
 
-    while (!pathAux.empty())
-    {
-        pair<ull, ull> coor = pathAux.top();
-        pathAux.pop();
-
-        finalPath.push_back(coor);
-    }
-    
+    reverse(finalPath.begin(), finalPath.end());
     
     return pair<double, vector<pair<ull, ull>>>(pathSize, finalPath);    
 }
