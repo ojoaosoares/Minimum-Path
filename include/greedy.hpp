c85ef52ce94c2944dad5a7a3569ce65319dada0a
joaosoares <ojoaosoaressantos@gmail.com>
2024-11-24 12:50:06 -0300

    Feat: greedy header.

---
+++ include/greedy.hpp
@@ -0,0 +1,17 @@
+#ifndef GREEDY_HPP
+#define GREEDY_HPP
+
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+
+typedef unsigned long long ull;
+
+using namespace std;
+
+double manhattanHeuristic(ull x1, ull y1, ull x2, ull y2);
+
+pair<double, vector<pair<ull, ull>>> greedy(const vector<vector<char>> &map, ull x1, ull y1, ull x2, ull y2);
+
+#endif
